d787489d7378b653c3d015ee5ebcfb838b4e4162
FH <fabrice.huet@inria.fr>
2013-06-18 18:04:18 +0200

    Resolves performance issue reported in #6

---
+++ mediastore-db/src/main/java/fr/thumbnailsdb/PreloadedDescriptors.java
@@ -0,0 +1,11 @@
+package fr.thumbnailsdb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fhuet
+ * Date: 18/06/13
+ * Time: 17:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class PreloadedDescriptors {
+}
