47169bc9508dd4fc234db13a4a36d568d9db5f41
BastienMaria <bastien.maria@gmail.com>
2014-06-09 17:33:10 +0200

    pagination de la galerie

--- mediastore-rest/src/main/webapp/js/thumbstore-gallery.js
+++ mediastore-rest/src/main/webapp/js/thumbstore-gallery.js
@@ -1,3 +1,4 @@
+var begin = 0;
 function getGallery() {
 
     var folders = getSelectedFolders();
@@ -5,16 +6,15 @@ function getGallery() {
         filter: $("input[name=filter_path]").val(),
         folder: JSON.stringify(folders),
         gps: false,
-        nb: 50
+        nb: 2
                 //$.param(folders)
     }, function(data) {
         buildGallery(data);
-        //   debugger;
+        begin = 2;
     });
 }
 
 function buildGallery(array) {
-
     $('#gallery').children().remove();
     for (f in array) {
         var tag = array[f];
@@ -22,7 +22,34 @@ function buildGallery(array) {
         ext = ext.toLowerCase();
 
         if (ext === '.jpeg' || ext === '.jpg' || ext === '.bmp' || ext === '.gif' || ext === '.png' || ext === '.tiff') {
-            $('#gallery').append("<a href='rest/hello/getThumbnail?path=" + tag.path + "&w=0&h=0' title='"+ tag.path +"' data-gallery=''><img src='rest/hello/getThumbnail?path=" + tag.path + "&w=70&h=70' alt='"+tag.path+"'></a>");
+            $('#gallery').append("<a href='rest/hello/getThumbnail?path=" + tag.path + "&w=0&h=0' title='" + tag.path + "' data-gallery=''><img src='rest/hello/getThumbnail?path=" + tag.path + "&w=70&h=70' alt='" + tag.path + "'></a>");
+        }
+    }
+}
+
+function appendPictures() {
+    var folders = getSelectedFolders();
+    $.getJSON('rest/hello/getAll', {
+        filter: $("input[name=filter_path]").val(),
+        folder: JSON.stringify(folders),
+        gps: false,
+        begin: begin,
+        nb: 2
+                //$.param(folders)
+    }, function(data) {
+        addPictures(data);
+        begin += 2;
+    });
+}
+
+function addPictures(array) {
+    for (f in array) {
+        var tag = array[f];
+        var ext = tag.path.substring(tag.path.lastIndexOf("."));
+        ext = ext.toLowerCase();
+
+        if (ext === '.jpeg' || ext === '.jpg' || ext === '.bmp' || ext === '.gif' || ext === '.png' || ext === '.tiff') {
+            $('#gallery').append("<a href='rest/hello/getThumbnail?path=" + tag.path + "&w=0&h=0' title='" + tag.path + "' data-gallery=''><img src='rest/hello/getThumbnail?path=" + tag.path + "&w=70&h=70' alt='" + tag.path + "'></a>");
         }
     }
 }
